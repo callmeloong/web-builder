54736e8fb24ce289f5406957f032e4ab5935b074
DESKTOP-DPMVQEQ\M710q <longth.dev@gmail.com>
2024-05-10 17:41:27 +0700

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import {
+  LucideAngularModule,
+  Menu,
+  Home,
+  UserCheck,
+  File,
+  BoxSelect,
+  Undo,
+  Redo,
+} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        File,
+        Home,
+        Menu,
+        UserCheck,
+        BoxSelect,
+        Undo,
+        Redo,
+      })
+    ),
+  ],
 };
